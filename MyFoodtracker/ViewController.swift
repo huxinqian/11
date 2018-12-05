0d9bfc932e45c8d419ba5201500d598d5d5d4f8b
student <student@imac-05.local>
2018-12-05 11:11:32 +0800

    Initial Commit

---
+++ MyFoodtracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFoodtracker
+//
+//  Created by student on 2018/12/5.
+//  Copyright © 2018年 2016110314. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
