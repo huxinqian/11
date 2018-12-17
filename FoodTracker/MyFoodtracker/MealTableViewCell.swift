e05acf94505ac6f7b48d0e753c549077a20f4082
student <student@imac-123.local>
2018-12-17 20:02:35 +0800

    ImplementNavigation

---
+++ FoodTracker/MyFoodtracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  MyFoodtracker
+//
+//  Created by student on 2018/12/17.
+//  Copyright © 2018年 2016110314. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
